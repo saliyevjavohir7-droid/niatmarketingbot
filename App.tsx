a32fee5b4ceab9c4799c88f5913de11f11c4edb6
saliyevjavohir7-droid <saliyevjavohir7@gmail.com>
2025-12-16 15:14:23 +0500

    Fix imports without components folder

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from './components/Hero';
-import Solutions from './components/Solutions';
-import Problems from './components/Problems';
-import NoResults from './components/NoResults';
-import Services from './components/Services';
-import LeadForm from './components/LeadForm';
-import Footer from './components/Footer';
+import Hero from './Hero';
+import Solutions from './Solutions';
+import Problems from './Problems';
+import NoResults from './NoResults';
+import Services from './Services';
+import LeadForm from './LeadForm';
+import Footer from './Footer';
 
 function App() {
   return (
